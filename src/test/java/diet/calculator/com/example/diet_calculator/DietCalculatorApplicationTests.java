cb48a238012db122ac54306e5f62f29f0cc8562a
Mateusz Piórowicz <marian@@>
2020-10-25 12:16:57 +0100

    Initial commit

---
+++ src/test/java/diet/calculator/com/example/diet_calculator/DietCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package diet.calculator.com.example.diet_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
